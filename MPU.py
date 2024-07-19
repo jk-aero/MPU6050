da8f8dc0882a6a50541a6f718a9fbf614517ceec
jk-aero <92267875+jk-aero@users.noreply.github.com>
2024-07-19 14:24:49 +0530

    Update MPU.py

--- MPU.py
+++ MPU.py
@@ -2,23 +2,6 @@ from machine import I2C, Pin
 import time
 from math import atan, degrees
 
-'''================================================================================================================================================='''
-'''
-The contents of this code and instructions are the intellectual property of Carbon Aeronautics. 
-The text and figures in this code and instructions are licensed under a Creative Commons Attribution - Noncommercial - ShareAlike 4.0 International Public Licence. 
-This license lets you remix, adapt, and build upon your work non-commercially, as long as you credit Carbon Aeronautics 
-(but not in any way that suggests that we endorse you or your use of the work) and license your new creations under the identical terms.
-This code and instruction is provided "As Is” without any further warranty. Neither Carbon Aeronautics or the author has any liability to any person or entity 
-with respect to any loss or damage caused or declared to be caused directly or indirectly by the instructions contained in this code or by 
-the software and hardware described in it. As Carbon Aeronautics has no control over the use, setup, assembly, modification or misuse of the hardware, 
-software and information described in this manual, no liability shall be assumed nor accepted for any resulting damage or injury. 
-By the act of copying, use, setup or assembly, the user accepts all resulting liability.
-
-1.0  29 December 2022 -  initial release
-'''
-
-"this  kalman filter was actually written in cpp I just changed it to a python class "
-
 class KalmanFilter:
     def __init__(self, KalmanState, KalmanUncertainity):
         self.KalmanState = KalmanState
